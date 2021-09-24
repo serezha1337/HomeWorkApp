29fdf82fef3f3dd758024ccd7642ef7ccf36e8f5
serezha1337 <razumooov@icloud.com>
2021-09-24 14:26:33 +0300

    Вторая домашка

--- src/main/java/HomeWork3.java
+++ src/main/java/HomeWork3.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class HomeWork3 {
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        //arrayFirstTask();
+        arrayFirstTask();
         //arraySecondTask();
         //arrayThirdTask();
         //squareArrayFourthTask();
